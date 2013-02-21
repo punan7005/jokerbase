6d4d846a8f44dcce791b48d38af73c4585b8a998
jokerPu <punan7005@qq.com>
2013-02-21 12:18:29 +0800

    Revert "Revert "jokerbase""
    This reverts commit 2f135b6821641580299f63c8c940a81d6cc8db33.

---
+++ src/com/jokerbase/business/dao/IUserDao.java
@@ -0,0 +1,13 @@
+package com.jokerbase.business.dao;
+
+import com.jokerbase.business.bean.User;
+import com.jokerbase.core.persistence.IGenericDao;
+
+/** 
+ * 类说明 :
+ * @author  joker 
+ * 创建时间：2013-2-1 下午4:31:18 
+ */
+public interface IUserDao extends IGenericDao<User, String>{
+	
+}
