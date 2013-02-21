f4b525e6913b7918f05cefe319f3e4428309eb2a
jokerPu <punan7005@qq.com>
2013-02-21 13:05:22 +0800

    create
    create jokerbase to github

---
+++ src/com/jokerbase/business/bean/Channel.java
@@ -0,0 +1,9 @@
+package com.jokerbase.business.bean;
+/** 
+ * 类说明 :栏目实体
+ * @author  joker 
+ * 创建时间：2013-2-20 下午3:18:37 
+ */
+public class Channel {
+	
+}
---
+++ src/com/jokerbase/business/service/IUserService.java
@@ -0,0 +1,13 @@
+package com.jokerbase.business.service;
+
+import com.jokerbase.business.bean.User;
+import com.jokerbase.core.service.IGenericService;
+
+/** 
+ * 类说明 :
+ * @author  joker 
+ * 创建时间：2013-2-19 上午11:10:21 
+ */
+public interface IUserService extends IGenericService<User, String>{
+
+}
